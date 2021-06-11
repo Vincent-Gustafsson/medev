3f34e17b8e88b0c432029f8a4a5c13e1ba8cbd84
Vincent <vincenthonor8lite@gmail.com>
2021-06-12 00:53:16 +0200

    Added Post model

--- medev/blogs/models.py
+++ medev/blogs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel
+
+
+class Post(BaseModel):
+    title = models.CharField(max_length=50, blank=False)
+    content = models.TextField(blank=False)
+    thumbnail = models.ImageField(upload_to='blogs/posts/thumbnail')
+
+    author = models.ForeignKey(
+        'User',
+        related_name='posts',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.title
